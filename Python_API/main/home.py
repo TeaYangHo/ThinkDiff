4460e6f51c862496f25b536eb8e565b5993c825d
TeaYangHo <95729211+TeaYangHo@users.noreply.github.com>
2023-08-02 08:25:03 +0700

    Update home.py

--- Python_API/main/home.py
+++ Python_API/main/home.py
@@ -1,5 +1,5 @@
-from .model import db, Users, Profiles, Anime_Manga_News, Reviews_Manga, Reviews_Anime, ListManga, ListChapter
-from . import func, convert_time, cast, Integer
+# from .model import db, Users, Profiles, Anime_Manga_News, Reviews_Manga, Reviews_Anime, List_Manga, List_Chapter, Manga_Update
+from . import *
 
 # from . import *
 
@@ -50,8 +50,9 @@ async def anime_manga_news():
 #REVIEWS MANGA
 async def reviews_manga():
 	data_reviews_manga = []
-	reviews_manga = Reviews_Manga.query.order_by(func.STR_TO_DATE(Reviews_Manga.time_review, "%b %d, %Y").desc())\
-		.limit(20).all()
+	reviews_manga = Reviews_Manga.query.order_by(func.STR_TO_DATE(Reviews_Manga.time_review, "%b %d, %Y")
+												.desc()).limit(20).all()
+
 	for review in reviews_manga:
 		data = {
 			"idReview": review.idReview,
@@ -66,10 +67,9 @@ async def reviews_manga():
 
 # REVIEWS ANIME
 async def reviews_anime():
-	# REVIEWS ANIME
 	data_reviews_anime = []
-	reviews_manga = Reviews_Anime.query.order_by(func.STR_TO_DATE(Reviews_Anime.time_review, "%b %d, %Y").desc())\
-		.limit(20).all()
+	reviews_manga = Reviews_Anime.query.order_by(func.STR_TO_DATE(Reviews_Anime.time_review, "%b %d, %Y")
+												.desc()).limit(20).all()
 	for review in reviews_manga:
 		data = {
 			"idReview": review.idReview,
@@ -85,15 +85,15 @@ async def reviews_anime():
 #RANK WEEK
 async def rank_manga_week():
 	data_rank_manga_week = []
-	rank_manga_week = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	rank_manga_week = Manga_Update.query.order_by(Manga_Update.views_week.desc()).limit(20).all()
 	for rank in rank_manga_week:
 		data = {
-			"id_manga": rank.id_manga,
+			"id_manga_original": rank.id_manga_original,
+			"id_manga_system": rank.id_manga_system,
 			"title_manga": rank.title_manga,
-			"image_poster_link_goc": rank.link_image_poster_link_goc,
-			"so_luong_view": rank.so_luong_view,
-			"list_categories": rank.list_categories,
-			"author": rank.tac_gia
+			"image_poster": rank.poster,
+			"categories": rank.categories,
+			"views_week": rank.views_week
 		}
 		data_rank_manga_week.append(data)
 	return data_rank_manga_week
@@ -101,15 +101,15 @@ async def rank_manga_week():
 #RANK MONTH
 async def rank_manga_month():
 	data_rank_manga_month = []
-	rank_manga_month = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	rank_manga_month = Manga_Update.query.order_by(Manga_Update.views_month.desc()).limit(20).all()
 	for rank in rank_manga_month:
 		data = {
-			"id_manga": rank.id_manga,
+			"id_manga_original": rank.id_manga_original,
+			"id_manga_system": rank.id_manga_system,
 			"title_manga": rank.title_manga,
-			"image_poster_link_goc": rank.link_image_poster_link_goc,
-			"so_luong_view": rank.so_luong_view,
-			"list_categories": rank.list_categories,
-			"author": rank.tac_gia
+			"image_poster": rank.poster,
+			"categories": rank.categories,
+			"views_month": rank.views_month
 		}
 		data_rank_manga_month.append(data)
 	return data_rank_manga_month
@@ -117,16 +117,15 @@ async def rank_manga_month():
 #RANK YEAR
 async def rank_manga_year():
 	data_rank_manga_year = []
-	rank_manga_year = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(
-		20).all()  # Views is digits
+	rank_manga_year = Manga_Update.query.order_by(Manga_Update.views.desc()).limit(20).all()
 	for rank in rank_manga_year:
 		data = {
-			"id_manga": rank.id_manga,
+			"id_manga_original": rank.id_manga_original,
+			"id_manga_system": rank.id_manga_system,
 			"title_manga": rank.title_manga,
-			"image_poster_link_goc": rank.link_image_poster_link_goc,
-			"so_luong_view": rank.so_luong_view,
-			"list_categories": rank.list_categories,
-			"author": rank.tac_gia
+			"image_poster": rank.poster,
+			"categories": rank.categories,
+			"views": rank.views
 		}
 		data_rank_manga_year.append(data)
 	return data_rank_manga_year
@@ -134,66 +133,53 @@ async def rank_manga_year():
 #COMEDY COMMICS
 async def comedy_comics():
 	data_comedy_comics = []
-	comedy_comics = ListManga.query.filter(ListManga.list_categories.like('%Comedy%')) \
-		.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	comedy_comics = Manga_Update.query.filter(Manga_Update.categories.like('%Comedy%')).\
+		order_by(func.STR_TO_DATE(Manga_Update.time_release, "%b %d, %Y").desc()).limit(20).all()
 	for comedy_comic in comedy_comics:
-		chapter_new = ListChapter.query.filter_by(id_manga=comedy_comic.id_manga) \
-			.order_by(func.STR_TO_DATE(ListChapter.thoi_gian_release, "%B %d, %Y").desc()).first()
-		if chapter_new is not None:
-			data = {
-				"id_manga": comedy_comic.id_manga,
-				"title_manga": comedy_comic.title_manga,
-				"image_poster_link_goc": comedy_comic.link_image_poster_link_goc,
-				"so_luong_view": comedy_comic.so_luong_view,
-				"list_categories": comedy_comic.list_categories,
-				"rate": comedy_comic.rate,
-				"author": comedy_comic.tac_gia,
-				"chapter_new": chapter_new.chapter,
-				"id_chapter": chapter_new.id_chapter,
-				"time_release": comedy_comic.time_release,
-			}
-			data_comedy_comics.append(data)
-		else:
-			continue
+		data = {
+			"id_manga": comedy_comic.id_manga_original,
+			"id_manga_system": comedy_comic.id_manga_system,
+			"title_manga": comedy_comic.title_manga,
+			"image_poster": comedy_comic.poster,
+			"rate": comedy_comic.rate,
+			"chapter_new": comedy_comic.title_chapter,
+			"id_chapter": comedy_comic.id_chapter_original,
+			"time_release": comedy_comic.time_release
+		}
+		data_comedy_comics.append(data)
 	return data_comedy_comics
 
 #FREE COMICS
 async def free_comics():
 	data_free_comics = []
-	free_comics = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	free_comics = Manga_Update.query.order_by(func.STR_TO_DATE(Manga_Update.time_release, "%b %d, %Y").desc()).limit(20).all()
 	for free_comic in free_comics:
-		chapter_new = ListChapter.query.filter_by(id_manga=free_comic.id_manga).order_by(
-			func.STR_TO_DATE(ListChapter.thoi_gian_release, "%B %d, %Y").desc()).first()
-		if chapter_new is not None:
-			data = {
-				"id_manga": free_comic.id_manga,
-				"title_manga": free_comic.title_manga,
-				"image_poster_link_goc": free_comic.link_image_poster_link_goc,
-				"so_luong_view": free_comic.so_luong_view,
-				"list_categories": free_comic.list_categories,
-				"rate": free_comic.rate,
-				"author": free_comic.tac_gia,
-				"chapter_new": chapter_new.chapter,
-				"id_chapter": chapter_new.id_chapter,
-				"time_release": free_comic.time_release,
-			}
-			data_free_comics.append(data)
-		else:
-			continue
+		data = {
+			"id_manga": free_comic.id_manga_original,
+			"title_manga": free_comic.title_manga,
+			"image_poster_link_goc": free_comic.poster,
+			"rate": free_comic.rate,
+			"chapter_new": free_comic.title_chapter,
+			"id_chapter": free_comic.id_chapter_original,
+			"time_release": free_comic.time_release
+		}
+		data_free_comics.append(data)
 	return data_free_comics
 
 #COOMING SOON COMICS
 async def cooming_soon_comics():
 	data_cooming_soon_comics = []
-	cooming_soon_comics = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	cooming_soon_comics = Manga_Update.query.\
+		order_by(func.STR_TO_DATE(Manga_Update.time_release, "%b %d, %Y").desc()).limit(20).all()
 	for cooming_soon_comic in cooming_soon_comics:
 		data = {
-			"id_manga": cooming_soon_comic.id_manga,
+			"id_manga_original": cooming_soon_comic.id_manga_original,
 			"title_manga": cooming_soon_comic.title_manga,
-			"image_poster": cooming_soon_comic.link_image_poster_link_goc,
-			"list_categories": cooming_soon_comic.list_categories,
-			"author": cooming_soon_comic.tac_gia,
-			"release_date": cooming_soon_comic.time_release,
+			"image_poster_link_goc": cooming_soon_comic.poster,
+			"rate": cooming_soon_comic.rate,
+			"chapter_new": cooming_soon_comic.title_chapter,
+			"id_chapter": cooming_soon_comic.id_chapter_original,
+			"time_release": cooming_soon_comic.time_release
 		}
 		data_cooming_soon_comics.append(data)
 	return data_cooming_soon_comics
@@ -201,74 +187,55 @@ async def cooming_soon_comics():
 #RECOMMENDED COMICS
 async def recommended_comics():
 	data_recommended_comics = []
-	recommended_comics = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	recommended_comics = Manga_Update.query.\
+		order_by(func.STR_TO_DATE(Manga_Update.time_release, "%b %d, %Y").desc()).limit(20).all()
 	for recommended_comic in recommended_comics:
-		chapter_new = ListChapter.query.filter_by(id_manga=recommended_comic.id_manga)\
-			.order_by(func.STR_TO_DATE(ListChapter.thoi_gian_release, "%B %d, %Y").desc()).first()
-		if chapter_new is not None:
-			data = {
-				"id_manga": recommended_comic.id_manga,
-				"title_manga": recommended_comic.title_manga,
-				"image_poster_link_goc": recommended_comic.link_image_poster_link_goc,
-				"so_luong_view": recommended_comic.so_luong_view,
-				"list_categories": recommended_comic.list_categories,
-				"rate": recommended_comic.rate,
-				"author": recommended_comic.tac_gia,
-				"chapter_new": chapter_new.chapter,
-				"id_chapter": chapter_new.id_chapter,
-				"time_release": recommended_comic.time_release,
-			}
-			data_recommended_comics.append(data)
-		else:
-			continue
+		data = {
+			"id_manga": recommended_comic.id_manga_original,
+			"title_manga": recommended_comic.title_manga,
+			"image_poster_link_goc": recommended_comic.poster,
+			"rate": recommended_comic.rate,
+			"chapter_new": recommended_comic.title_chapter,
+			"id_chapter": recommended_comic.id_chapter_original,
+			"time_release": recommended_comic.time_release
+		}
+		data_recommended_comics.append(data)
+	return data_recommended_comics
 
 #RECENT COMICS
 async def recent_comics():
 	data_recent_comics = []
-	recent_comics = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	recent_comics = Manga_Update.query.\
+		order_by(func.STR_TO_DATE(Manga_Update.time_release, "%b %d, %Y").desc()).limit(20).all()
 	for recent_comic in recent_comics:
-		chapter_new = ListChapter.query.filter_by(id_manga=recent_comic.id_manga) \
-			.order_by(func.STR_TO_DATE(ListChapter.thoi_gian_release, "%B %d, %Y").desc()).first()
-		if chapter_new is not None:
-			data = {
-				"id_manga": recent_comic.id_manga,
-				"title_manga": recent_comic.title_manga,
-				"image_poster_link_goc": recent_comic.link_image_poster_link_goc,
-				"so_luong_view": recent_comic.so_luong_view,
-				"list_categories": recent_comic.list_categories,
-				"rate": recent_comic.rate,
-				"author": recent_comic.tac_gia,
-				"chapter_new": chapter_new.chapter,
-				"id_chapter": chapter_new.id_chapter,
-				"time_release": recent_comic.time_release,
-			}
-			data_recent_comics.append(data)
+		data = {
+			"id_manga": recent_comic.id_manga_original,
+			"title_manga": recent_comic.title_manga,
+			"image_poster_link_goc": recent_comic.poster,
+			"rate": recent_comic.rate,
+			"chapter_new": recent_comic.title_chapter,
+			"id_chapter": recent_comic.id_chapter_original,
+			"time_release": recent_comic.time_release
+		}
+		data_recent_comics.append(data)
 
-		else:
-			continue
 	return data_recent_comics
 
 #NEW RELEASE COMICS
 async def new_release_comics():
 	data_new_release_comics = []
-	new_release_comics = ListManga.query.order_by(cast(ListManga.so_luong_view, Integer).desc()).limit(20).all()
+	new_release_comics = Manga_Update.query.\
+		order_by(func.STR_TO_DATE(Manga_Update.time_release, "%b %d, %Y").desc()).limit(20).all()
 	for new_release_comic in new_release_comics:
-		chapter_new = ListChapter.query.filter_by(id_manga=new_release_comic.id_manga) \
-			.order_by(func.STR_TO_DATE(ListChapter.thoi_gian_release, "%B %d, %Y").desc()).first()
-		if chapter_new is not None:
-			data = {
-				"id_manga": new_release_comic.id_manga,
-				"title_manga": new_release_comic.title_manga,
-				"image_poster_link_goc": new_release_comic.link_image_poster_link_goc,
-				"so_luong_view": new_release_comic.so_luong_view,
-				"list_categories": new_release_comic.list_categories,
-				"rate": new_release_comic.rate,
-				"author": new_release_comic.tac_gia,
-				"chapter_new": chapter_new.chapter,
-				"id_chapter": chapter_new.id_chapter,
-				"time_release": new_release_comic.time_release,
-			}
-			data_new_release_comics.append(data)
-		else:
-			continue
+		data = {
+			"id_manga": new_release_comic.id_manga_original,
+			"title_manga": new_release_comic.title_manga,
+			"image_poster_link_goc": new_release_comic.poster,
+			"rate": new_release_comic.rate,
+			"chapter_new": new_release_comic.title_chapter,
+			"id_chapter": new_release_comic.id_chapter_original,
+			"time_release": new_release_comic.time_release
+		}
+		data_new_release_comics.append(data)
+
 	return data_new_release_comics
